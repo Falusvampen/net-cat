373064ec75696030bae5c2372ecfc7fbe1366136
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-08 19:02:38 +0200

    Final main, checks if port is valid

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net-cat/server"
 	"os"
 )
 
 var Port = "8989"
+
 // run the server on the given port number
 func main() {
-	// go run . 8546
-	if len(os.Args) > 1 {
-		Port = os.Args[1]
+	// go run . h354s
+	if len(os.Args) == 1 {
+		server.StartServer(Port)
 	}
-	server.StartServer(Port)
+	if len(os.Args) == 2 {
+		false := server.CheckValidPort(os.Args[1])
+		if false {
+			server.StartServer(os.Args[1])
+		}
+	} else {
+		fmt.Println("Too many arguments")
+		fmt.Println("[USAGE]: ./TCPChat $port")
+	}
 }
